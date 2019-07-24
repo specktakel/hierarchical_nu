85479d1221e2172f4934cf121e817b2b3689fd3c
Francesca Capel <capel.francesca@gmail.com>
2019-07-24 16:37:05 +0200

    Add Aeff factor in for the cascades - tracks need more work

---
+++ dev/statistical_model/5_aeff_factor/aeff_calculator/aeff_info.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+class aeff_info(object):
+    def __init__(self):
+
+        self.nue_cc = './aeff_input_cascades/effective_area.per_bin.nu_e.cc.cascade.txt'
+        self.nue_cc_smoothing = 1
+
+        self.nue_nc = './aeff_input_cascades/effective_area.per_bin.nu_e.nc.cascade.txt'
+        self.nue_nc_smoothing = 5
+
+        self.nue_gr = './aeff_input_cascades/effective_area.per_bin.nu_e_bar.gr.cascade.txt'
+        self.nue_gr_smoothing = 15
+
+        self.numu_cc = './aeff_input_cascades/effective_area.per_bin.nu_mu.cc.cascade.txt'
+        self.numu_cc_smoothing = 150
+
+        self.numu_nc = './aeff_input_cascades/effective_area.per_bin.nu_mu.nc.cascade.txt'
+        self.numu_nc_smoothing = 300
+
+        self.nutau_cc = './aeff_input_cascades/effective_area.per_bin.nu_tau.cc.cascade.txt'
+        self.nutau_cc_smoothing = 5
+
+        self.nutau_nc = './aeff_input_cascades/effective_area.per_bin.nu_tau.nc.cascade.txt'
+        self.nutau_nc_smoothing = 30
+
+
+        self.nuebar_cc = './aeff_input_cascades/effective_area.per_bin.nu_e_bar.cc.cascade.txt'
+        self.nuebar_cc_smoothing = 1
+
+        self.nuebar_nc = './aeff_input_cascades/effective_area.per_bin.nu_e_bar.nc.cascade.txt'
+        self.nuebar_nc_smoothing = 5
+
+        self.nuebar_gr = './aeff_input_cascades/effective_area.per_bin.nu_e_bar_bar.gr.cascade.txt'
+        self.nuebar_gr_smoothing = 15
+
+        self.numubar_cc = './aeff_input_cascades/effective_area.per_bin.nu_mu_bar.cc.cascade.txt'
+        self.numubar_cc_smoothing = 150
+
+        self.numubar_nc = './aeff_input_cascades/effective_area.per_bin.nu_mu_bar.nc.cascade.txt'
+        self.numubar_nc_smoothing = 300
+
+        self.nutaubar_cc = './aeff_input_cascades/effective_area.per_bin.nu_tau_bar.cc.cascade.txt'
+        self.nutaubar_cc_smoothing = 5
+
+        self.nutaubar_nc = './aeff_input_cascades/effective_area.per_bin.nu_tau_bar.nc.cascade.txt'
+        self.nutaubar_nc_smoothing = 30
+
+        '''
+        self.nue_cc = '../aeff_input_cascades/effective_area.per_bin.nu_e.cc.cascade.txt'
+        self.nue_cc_smoothing = 1
+
+        self.nue_nc = '../aeff_input_cascades/effective_area.per_bin.nu_e.nc.cascade.txt'
+        self.nue_nc_smoothing = 5
+
+        self.nue_gr = '../aeff_input_cascades/effective_area.per_bin.nu_e_bar.gr.cascade.txt'
+        self.nue_gr_smoothing = 15
+
+        self.numu_cc = '../aeff_input_cascades/effective_area.per_bin.nu_mu.cc.cascade.txt'
+        self.numu_cc_smoothing = 150
+
+        self.numu_nc = '../aeff_input_cascades/effective_area.per_bin.nu_mu.nc.cascade.txt'
+        self.numu_nc_smoothing = 300
+
+        self.nutau_cc = '../aeff_input_cascades/effective_area.per_bin.nu_tau.cc.cascade.txt'
+        self.nutau_cc_smoothing = 5
+
+        self.nutau_nc = '../aeff_input_cascades/effective_area.per_bin.nu_tau.nc.cascade.txt'
+        self.nutau_nc_smoothing = 30
+        '''
+
+        self.infiles = {'nue_NC': self.nue_nc, 'nue_CC': self.nue_cc, 'numu_NC': self.numu_nc, 'numu_CC': self.numu_cc, 'nutau_NC': self.nutau_nc, 'nutau_CC': self.nutau_cc, 'nue_GR': self.nue_gr,
+                'nuebar_NC': self.nuebar_nc, 'nuebar_CC': self.nuebar_cc, 'numubar_NC': self.numubar_nc, 'numubar_CC': self.numubar_cc, 'nutaubar_NC': self.nutaubar_nc, 'nutaubar_CC': self.nutaubar_cc, 'nuebar_GR': self.nuebar_gr}
+        self.smoothing = {'nue_NC': self.nue_nc_smoothing, 'nue_CC': self.nue_cc_smoothing, 'numu_NC': self.numu_nc_smoothing, 'numu_CC': self.numu_cc_smoothing, 'nutau_NC': self.nutau_nc_smoothing, 'nutau_CC': self.nutau_cc_smoothing, 'nue_GR': self.nue_gr_smoothing,
+                'nuebar_NC': self.nuebar_nc_smoothing, 'nuebar_CC': self.nuebar_cc_smoothing, 'numubar_NC': self.numubar_nc_smoothing, 'numubar_CC': self.numubar_cc_smoothing, 'nutaubar_NC': self.nutaubar_nc_smoothing, 'nutaubar_CC': self.nutaubar_cc_smoothing, 'nuebar_GR': self.nuebar_gr_smoothing}
+
---
+++ dev/statistical_model/5_aeff_factor/exposure_integral.py
@@ -0,0 +1,126 @@
+import numpy as np
+from scipy import integrate
+import h5py
+from tqdm.autonotebook import tqdm as progress_bar
+
+# Maximum energy in GeV
+Emax = 1.0E7
+
+# Convert 3D unit vector to cos(zenith) value
+def unit_vector_to_cosz(unit_vector):
+    return np.cos(np.pi - np.arccos(unit_vector[2]))
+
+class ExposureIntegral(object):
+    """
+    Everything you need for precomputing exposure integrals.
+    
+    @author Francesca Capel
+    @date April 2019
+    """
+
+    def __init__(self, source_redshift, source_position, bg_redshift, effective_area, Emin, filename=None, n_points=50):
+        """
+        Everything you need for precomputing exposure integrals.
+        
+        :param source_redshift: list of source redshifts.
+        :param source_position: list of unit vectors representing source positions.
+        :param bg_redshift: single background redshift.
+        :param effective_area: effective_area object from aeff_calculator.
+        :param Emin: minimum energy of sample in GeV.
+        :param filename: file to save to.
+        :param n_points: nummber of points to evaluate integral on.
+        """
+
+        self.source_redshift = source_redshift
+        self.source_position = source_position
+        self.bg_redshift = bg_redshift
+        self.Aeff = effective_area
+        self.Emin = Emin
+        self.filename = filename
+
+        self.alpha_grid = np.logspace(np.log(1.0), np.log(4.0), n_points, base=np.e)
+        
+        self._calculate_exposure_integral()
+        self._save_to_file()
+
+        
+    def _source_integrand(self, E, position, z, alpha):
+        """
+        Integrand for point sources.
+        """
+
+        log10E = np.log10(E) # GeV
+        cosz = unit_vector_to_cosz(position)
+        Aeff = np.power(10, self.Aeff.eval(log10E, cosz)[0][0]) # m^2
+
+        return  Aeff * ( ((1+z)*E) / self.Emin )**(-alpha)
+
+    
+    def _bg_integrand(self, E, cosz, z, alpha):
+        """
+        Integrand for isotropic background at a certain redshift.
+        """
+
+        log10E = np.log10(E) # GeV
+        Aeff = np.power(10, self.Aeff.eval(log10E, cosz)[0][0])
+
+        return Aeff * ( ((1+z)*E) / self.Emin )**(-alpha)
+
+    
+    def _get_source_integral(self, position, z, alpha):
+
+        integ, err = integrate.quad(self._source_integrand, self.Emin, Emax,
+                                    args=(position, z, alpha))
+        # debug
+        #print(integ, err)
+        return integ
+
+    
+    def _get_bg_integral(self, z, alpha):
+
+        integ, err = integrate.dblquad(self._bg_integrand, -1, 1,
+                                       lambda E: self.Emin, lambda E: Emax,
+                                       args=(z, alpha))
+        # debug
+        #print(integ, err)    
+        return integ * 0.5 # factor of 2pi/4pi
+    
+    
+    def _calculate_exposure_integral(self):
+        """
+        Run calculation.
+        """
+
+        # Sources
+        self.integral_grid = []
+        for i in progress_bar(range(len(self.source_position)), desc='Source integrals'):
+            position = self.source_position[i]
+            z = self.source_redshift[i]
+            I_k = []
+            for alpha in self.alpha_grid:
+                I_k.append(self._get_source_integral(position, z, alpha))
+            self.integral_grid.append(I_k)
+
+        # Background
+        I_k = []
+        for i in progress_bar(range(len(self.alpha_grid)), desc='Background integrals'):
+            alpha = self.alpha_grid[i] 
+            I_k.append(self._get_bg_integral(self.bg_redshift, alpha))
+        self.integral_grid.append(I_k)
+
+        
+    def _save_to_file(self):
+        """
+        Save to file if self.filename is defined.
+        """
+
+        if self.filename:
+
+            with h5py.File(self.filename, 'w') as f:
+                f.create_dataset('integral_grid', data=self.integral_grid)
+                f.create_dataset('alpha_grid', data=self.alpha_grid)
+                f.create_dataset('Emin', data=self.Emin)
+                
+
+        
+
