680c6ae88adf47aae071156d041c55f065abe160
Francesca Capel <capel.francesca@gmail.com>
2019-07-25 17:06:12 +0200

    Start software writing!

---
+++ software/eventlist.py
@@ -0,0 +1,16 @@
+"""
+EventList
+
+@author Francesca Capel
+@date July 2019
+"""
+
+class EventList(object):
+    """
+    Coming soon!
+    """
+
+    def __init__():
+        """
+        Coming soon!
+        """
