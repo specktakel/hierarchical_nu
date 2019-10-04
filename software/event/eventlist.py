749f704a88c9b0b2e59264678f8697ed134ad1db
Francesca Capel <capel.francesca@gmail.com>
2019-10-04 14:10:28 +0200

    Add source classes

---
+++ software/event/eventlist.py
@@ -0,0 +1,16 @@
+"""
+EventList class.
+
+@author Francesca Capel
+@date July 2019
+"""
+
+class EventList(object):
+    """
+    EventList class for the storage of event observable
+    """
+
+    def __init__():
+        """
+        Coming soon!
+        """
