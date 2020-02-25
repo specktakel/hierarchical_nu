Fix missing factor of 4pi in background integral

software/Precomputation.py
@@ -134,7 +134,7 @@ class ExposureIntegral():
  
                             aeff = self.effective_area._eff_area[j][np.digitize(cosz, self.effective_area._cosz_bin_edges)-1]
  
-                        integral_grid_tmp[i] += integ * aeff * np.power(1+z, 1-alpha) 
+                        integral_grid_tmp[i] += integ * aeff * np.power(1+z, 1-alpha) # GeV m^2 
 
                         j += 1
 
@@ -160,7 +160,7 @@ class ExposureIntegral():
                                 
                                 aeff = self.effective_area._eff_area[j][k] 
 
-                            integral_grid_tmp[i] += E_integ * ang_integ * aeff * np.power(1+z, 1-alpha)
+                            integral_grid_tmp[i] += E_integ * (ang_integ/(4*np.pi)) * aeff * np.power(1+z, 1-alpha) # GeV m^2 sr
 
                             k += 1
 