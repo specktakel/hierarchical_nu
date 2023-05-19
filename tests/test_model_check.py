3b0d5ce740de9a88d717640863d494ca058b7df1
Francesca Capel <capel.francesca@gmail.com>
2023-05-19 12:30:34 +0200

    Add test for old detector model

--- tests/test_model_check.py
+++ tests/test_model_check.py
@@ -1,15 +1,16 @@
 import os
+from omegaconf import OmegaConf
 
 from hierarchical_nu.model_check import ModelCheck
+from hierarchical_nu.utils.config import hnu_config
+from hierarchical_nu.utils.config import _local_config_file
 
 
 def test_initialisation(output_directory):
-
     ModelCheck.initialise_env(output_dir=output_directory)
 
 
-def test_short_run(output_directory, random_seed):
-
+def run_model_check(output_directory, random_seed):
     n_jobs = 1
     n_subjobs = 1
 
@@ -27,11 +28,40 @@ def test_short_run(output_directory, random_seed):
     file_list = [output_file]
     model_check = ModelCheck.load(file_list)
 
+    return model_check
+
+
+def test_short_run_r2021(output_directory, random_seed):
+    # Edit configuration and save
+    hnu_config["parameter_config"]["detector_model_type"] = "r2021"
+
+    with _local_config_file.open("w") as f:
+        OmegaConf.save(config=hnu_config, f=f.name)
+
+    model_check = run_model_check(output_directory, random_seed)
+
     # Check diagnostics
     ind_not_ok = model_check.diagnose()
     assert len(ind_not_ok) == 0
 
     # Check visualisations
     for p in [True, False]:
+        fig, ax = model_check.compare(show_prior=p)
 
+
+def test_short_run_icecube(output_directory, random_seed):
+    # Edit configuration and save
+    hnu_config["parameter_config"]["detector_model_type"] = "icecube"
+
+    with _local_config_file.open("w") as f:
+        OmegaConf.save(config=hnu_config, f=f.name)
+
+    model_check = run_model_check(output_directory, random_seed)
+
+    # Check diagnostics
+    ind_not_ok = model_check.diagnose()
+    assert len(ind_not_ok) == 0
+
+    # Check visualisations
+    for p in [True, False]:
         fig, ax = model_check.compare(show_prior=p)
