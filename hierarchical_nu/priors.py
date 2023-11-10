6f449d0fe6b101cc6e7077763173ec3069a3e0ee
Julian Kuhlmann <kuhlmann@mpp.mpg.de>
2023-11-10 15:08:21 +0100

    weird idea

--- hierarchical_nu/priors.py
+++ hierarchical_nu/priors.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+import astropy.units as u
 import numpy as np
 from scipy import stats
 import h5py
@@ -11,52 +12,41 @@ class PriorDistribution(metaclass=ABCMeta):
     """
 
     def __init__(self, name="prior"):
-
         self._name = name
 
     @property
     def name(self):
-
         return self._name
 
     @abstractmethod
     def pdf(self, x):
-
         pass
 
     def pdf_logspace(self, x):
-        
         return self.pdf(x) * x * np.log(10)
 
     @abstractmethod
     def sample(self, N):
-
         pass
 
     @abstractmethod
     def to_dict(self):
-
         pass
 
     @classmethod
     def from_dict(cls, prior_dict):
-
         name = prior_dict["name"]
 
         if name == "normal":
-
             prior = NormalPrior(**prior_dict)
 
         elif name == "lognormal":
-
             prior = LogNormalPrior(**prior_dict)
 
         elif name == "pareto":
-
             prior = ParetoPrior(**prior_dict)
 
         else:
-
             raise ValueError("Prior distribution type not recognised")
 
         return prior
@@ -64,6 +54,8 @@ class PriorDistribution(metaclass=ABCMeta):
 
 class NormalPrior(PriorDistribution):
     def __init__(self, name="normal", mu=0.0, sigma=1.0):
+        if isinstance(mu, u.Quantity) or isinstance(sigma, u.Quantity):
+            assert mu.is_equivalent(sigma)
 
         super().__init__(name=name)
 
@@ -72,24 +64,19 @@ class NormalPrior(PriorDistribution):
 
     @property
     def mu(self):
-
         return self._mu
 
     @property
     def sigma(self):
-
         return self._sigma
 
     def pdf(self, x):
-
         return stats.norm(loc=self._mu, scale=self._sigma).pdf(x)
 
     def sample(self, N):
-
         return stats.norm(loc=self._mu, scale=self._sigma).rvs(N)
 
     def to_dict(self):
-
         prior_dict = {}
 
         prior_dict["name"] = self._name
@@ -103,21 +90,17 @@ class NormalPrior(PriorDistribution):
 
 class LogNormalPrior(NormalPrior):
     def __init__(self, name="lognormal", mu=1.0, sigma=1.0):
-
         super().__init__(name=name, mu=mu, sigma=sigma)
 
     def pdf(self, x):
-
         return stats.lognorm(scale=np.exp(self._mu), s=self._sigma).pdf(x)
 
     def sample(self, N):
-
         return stats.lognorm(scale=np.exp(self._mu), s=self._sigma).rvs(N)
 
 
 class ParetoPrior(PriorDistribution):
     def __init__(self, name="pareto", xmin=1.0, alpha=1.0):
-
         super().__init__(name=name)
 
         self._xmin = xmin
@@ -125,24 +108,19 @@ class ParetoPrior(PriorDistribution):
 
     @property
     def xmin(self):
-
         return self._xmin
 
     @property
     def alpha(self):
-
         return self._alpha
 
     def pdf(self, x):
-
         return stats.pareto(b=self._alpha).pdf(x)
 
     def sample(self, N):
-
         return stats.pareto(b=self._alpha).rvs(N) * self._xmin
 
     def to_dict(self):
-
         prior_dict = {}
 
         prior_dict["name"] = self._name
@@ -154,6 +132,13 @@ class ParetoPrior(PriorDistribution):
         return prior_dict
 
 
+class Luminosity(PriorDistribution):
+    def __init__(
+        self, name="LogNormal", mu=1e49 * u.GeV / u.s, sigma=np.exp(3) * u.GeV / u.s
+    ):
+        pass
+
+
 class Priors(object):
     """
     Container for model priors.
@@ -163,69 +148,62 @@ class Priors(object):
     """
 
     def __init__(self):
+        self.luminosity = LogNormalPrior(mu=np.log(1e50), sigma=10.0)
 
-        self._luminosity = LogNormalPrior(mu=np.log(1e50), sigma=10.0)
+        self.diffuse_flux = LogNormalPrior(mu=np.log(1e-7), sigma=1.0)
 
-        self._diffuse_flux = LogNormalPrior(mu=np.log(1e-7), sigma=1.0)
+        self.src_index = NormalPrior(mu=2.0, sigma=1.5)
 
-        self._src_index = NormalPrior(mu=2.0, sigma=1.5)
+        self.diff_index = NormalPrior(mu=2.5, sigma=1.5)
 
-        self._diff_index = NormalPrior(mu=2.5, sigma=1.5)
-
-        self._atmospheric_flux = LogNormalPrior(mu=np.log(1e-5), sigma=1.0)
+        self.atmospheric_flux = LogNormalPrior(mu=np.log(1e-5), sigma=1.0)
 
     @property
     def luminosity(self):
-
         return self._luminosity
 
     @luminosity.setter
     def luminosity(self, prior: PriorDistribution):
+        units = u.GeV / u.s
+        if not isinstance(prior, ParetoPrior):
+            prior._mu = prior._mu.to(units)
+            sigma = prior.sigma
 
         self._luminosity = prior
 
     @property
     def diffuse_flux(self):
-
         return self._diffuse_flux
 
     @diffuse_flux.setter
     def diffuse_flux(self, prior: PriorDistribution):
-
         self._diffuse_flux = prior
 
     @property
     def src_index(self):
-
         return self._src_index
 
     @src_index.setter
     def src_index(self, prior: PriorDistribution):
-
         self._src_index = prior
 
     @property
     def diff_index(self):
-
         return self._diff_index
 
     @diff_index.setter
     def diff_index(self, prior: PriorDistribution):
-
         self._diff_index = prior
 
     @property
     def atmospheric_flux(self):
-
         return self._atmospheric_flux
 
     @atmospheric_flux.setter
     def atmospheric_flux(self, prior: PriorDistribution):
-
         self._atmospheric_flux = prior
 
     def to_dict(self):
-
         priors_dict = {}
 
         priors_dict["L"] = self._luminosity
@@ -241,64 +219,48 @@ class Priors(object):
         return priors_dict
 
     def save(self, file_name: str):
-
         with h5py.File(file_name, "w") as f:
-
             self._writeto(f)
 
     def _writeto(self, f):
-
         priors_dict = self.to_dict()
 
         for key, value in priors_dict.items():
-
             g = f.create_group(key)
 
             for key, value in value.to_dict().items():
-
                 g.create_dataset(key, data=value)
 
     def addto(self, file_name: str, group_name: str):
-
         with h5py.File(file_name, "r+") as f:
-
             g = f.create_group(group_name)
 
             self._writeto(g)
 
     @classmethod
     def from_file(cls, file_name: str):
-
         with h5py.File(file_name, "r") as f:
-
             return cls._load_from(f)
 
     @classmethod
     def from_group(cls, file_name: str, group_name: str):
-
         with h5py.File(file_name, "r") as f:
-
             g = f[group_name]
 
             return cls._load_from(g)
 
     @classmethod
     def _load_from(cls, f):
-
         priors_dict = {}
 
         for key, value in f.items():
-
             prior_dict = {}
 
             for k, v in value.items():
-
                 if k == "name":
-
                     prior_dict[k] = v[()].decode("ascii")
 
                 else:
-
                     prior_dict[k] = v[()]
 
             priors_dict[key] = PriorDistribution.from_dict(prior_dict)
@@ -307,7 +269,6 @@ class Priors(object):
 
     @classmethod
     def from_dict(cls, priors_dict):
-
         priors = cls()
 
         priors.luminosity = priors_dict["L"]
