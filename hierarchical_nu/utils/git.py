88afa2ad8f753c6d356e47bba17ab9178f1e4ea5
Julian Kuhlmann <kuhlmann@mpp.mpg.de>
2024-03-13 15:22:57 +0100

    add version for pip installation

--- hierarchical_nu/utils/git.py
+++ hierarchical_nu/utils/git.py
@@ -8,8 +8,17 @@ from .lifetime import LifeTime
 # get dir of arbitrary class
 cwd = path.abspath(path.dirname(inspect.getfile(LifeTime)))
 
-
-process = subprocess.Popen(
-    ["git", "rev-parse", "HEAD"], shell=False, stdout=subprocess.PIPE, cwd=cwd
-)
-git_hash = process.communicate()[0].strip().decode("ascii")
+try:
+    process = subprocess.Popen(
+        ["git", "rev-parse", "HEAD"], shell=False, stdout=subprocess.PIPE, cwd=cwd
+    )
+    git_hash = process.communicate()[0].strip().decode("ascii")
+    if "fatal" in git_hash:
+        # we are not using git but a pip installation
+        raise ValueError
+except ValueError:
+    process = subprocess.Popen(
+        ["pip", "show", "hierarchical_nu"], shell=False, stdout=subprocess.PIPE, cwd=cwd
+    )
+    git_hash = process.communicate()[0].strip().decode("ascii")
+    git_hash = git_hash.split("\n")[1].lstrip("Version: ")
