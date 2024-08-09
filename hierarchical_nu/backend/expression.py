2aef50fab828c434da7f76f1ef382ea2fd1cb8d7
Julian Kuhlmann <kuhlmann@mpp.mpg.de>
2024-08-09 16:10:41 +0200

    does this change anything?

--- hierarchical_nu/backend/expression.py
+++ hierarchical_nu/backend/expression.py
@@ -117,7 +117,7 @@ class Expression(_BaseExpression):
 
         with IndexingContext(key) as idx:
             output: TListTExpression = [self, *idx]
-        return Expression([self, key], output)
+        return Expression([self, key], output, end_delim="")
 
     """
 
